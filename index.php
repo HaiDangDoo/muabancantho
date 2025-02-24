575392b544dc56eac18951eb6038f6cfa519c01b
haidangdoo <haidangle884@gmail.com>
2025-02-24 21:02:19 +0700

    lệnh mới

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ban Ca</title>
+</head>
+<body>
+    <h1>Mới tải xong git và tortoi</h1>
+</body>
+</html>
